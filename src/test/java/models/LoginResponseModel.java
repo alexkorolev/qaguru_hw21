b804d68167329fdbda0e69f8e48283c31f816dd5
Aleksei Korolev <alexkorolev86@gmail.com>
2024-02-04 11:46:57 +0300

    first commit

---
+++ src/test/java/models/LoginResponseModel.java
@@ -0,0 +1,15 @@
+package models;
+
+public class LoginResponseModel {
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    String token;
+
+}
